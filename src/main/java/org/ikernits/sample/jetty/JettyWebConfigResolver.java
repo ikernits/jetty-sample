90bce5b43d2ec5e8d262e77bb016c441f1ceadda
ikernits <ikernits@gmail.com>
2015-12-07 04:06:08 +0300

    updated web config resolver

--- src/main/java/org/ikernits/sample/jetty/JettyWebConfigResolver.java
+++ src/main/java/org/ikernits/sample/jetty/JettyWebConfigResolver.java
@@ -1,13 +1,30 @@
 package org.ikernits.sample.jetty;
 
+import org.springframework.beans.factory.annotation.Required;
+
 import java.net.URL;
 
 public class JettyWebConfigResolver {
+    private static String configName = "web.xml";
+
+    @Required
+    public void setConfigName(String configName) {
+        JettyWebConfigResolver.configName = configName;
+    }
+
     public String getConfigLocation() {
         URL warUrl = Thread.currentThread().getContextClassLoader().getResource("WEB-INF");
         if (warUrl == null) {
             throw new IllegalStateException("Failed to locate classpath root");
         }
         return warUrl.toString().substring(0, warUrl.toString().length() - 7);
+    }
+
+    public String getDescriptorName() {
+        URL configUrl = Thread.currentThread().getContextClassLoader().getResource("WEB-INF/" + configName);
+        if (configUrl == null) {
+            throw new IllegalStateException("Failed to web application descriptor " + configName);
+        }
+        return configUrl.toString();
     }
 }
